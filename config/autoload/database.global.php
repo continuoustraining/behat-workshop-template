646b562690398c454b18f6bff038d59d146c8c66
ppaulis <ppaulis@gmail.com>
2016-05-14 22:00:55 +0200

    loaded Doctrine modules

--- config/autoload/database.global.php
+++ config/autoload/database.global.php
@@ -12,6 +12,25 @@ return [
                                     'path' => __DIR__ . '/../../data/ecommerce.db'
                                 ]
                         ]
-                ]
+                ],
+            'driver' =>
+                [
+                    'Ecommerce_Driver' =>
+                        [
+                            'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                            'cache' => 'array',
+                            'paths' =>
+                                [
+                                    __DIR__ . '/../../module/Ecommerce/src/Ecommerce/V1'
+                                ]
+                        ],
+                    'orm_default' =>
+                        [
+                            'drivers' =>
+                                [
+                                    'App' => 'Ecommerce_Driver',
+                                ]
+                        ]
+                ],
         ]
 ];
