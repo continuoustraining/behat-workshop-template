0a46995fb51a53bf9dae927614be61af6e8242fc
ppaulis <ppaulis@gmail.com>
2016-05-19 17:05:14 +0200

    ex-02: creating Behat setup

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,40 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Behat context class.
+ */
+class FeatureContext implements SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets it's own context object.
+     * You can also pass arbitrary arguments to the context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        require_once(__DIR__ . '/../../vendor/autoload.php');
+
+        ini_set('memory_limit', '-1');
+
+        $this->zf2MvcApplication = \Zend\Mvc\Application::init(require __DIR__ . '/../../config/application.config.php');
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->getServiceManager()->get('entity_manager');
+    }
+
+    public function getServiceManager()
+    {
+        return $this->zf2MvcApplication->getServiceManager();
+    }
+}
