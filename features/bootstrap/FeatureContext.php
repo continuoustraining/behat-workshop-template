d39b2f94a369924584d273c7d2f44e8bdda7b2e7
ppaulis <ppaulis@gmail.com>
2016-05-19 21:02:26 +0200

    added scenarios

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -64,7 +64,7 @@ class FeatureContext implements SnippetAcceptingContext
         }
 
         $mapperUsers->store($user)
-                    ->flush($user);
+            ->flush($user);
     }
 
     /**
@@ -79,12 +79,68 @@ class FeatureContext implements SnippetAcceptingContext
         if (count($users) != 1) {
             throw new \Exception('Exactly 1 user should have been created. Found: ' . count($users));
         }
-        
+
         foreach ($table->getRows() as $property) {
             $getter = 'get' . ucfirst($property[0]);
             if ($property[1] != $users[0]->$getter()) {
                 throw new \Exception('Resource not found.');
             }
         }
+    }
+
+    /**
+     * @Then the :arg1 with the following data should have been deleted:
+     */
+    public function theWithTheFollowingDataShouldHaveBeenDeleted($entityName, TableNode $table)
+    {
+        /** @var \Ecommerce\V1\Rest\Users\UsersMapper $mapperUsers */
+        $mapperUsers = $this->getServiceManager()->get('mapper.' . $entityName);
+        $users = $mapperUsers->findAll();
+
+        /** @var \Ecommerce\V1\Rest\Users\UsersEntity $user */
+        foreach ($users as $user) {
+            $found = true;
+
+            foreach ($table->getRows() as $property) {
+                $getter = 'get' . ucfirst($property[0]);
+                
+                if ($property[1] != $user->$getter()) {
+                    $found = false;
+                    break;
+                }
+            }
+
+            if ($found) {
+                throw new \Exception("The $entityName should have been deleted.");
+            }
+        }
+    }
+
+    /**
+     * @Then the :arg1 with the following data should exist:
+     */
+    public function theWithTheFollowingDataShouldExist($entityName, TableNode $table)
+    {
+        /** @var \Ecommerce\V1\Rest\Users\UsersMapper $mapperUsers */
+        $mapperUsers = $this->getServiceManager()->get('mapper.' . $entityName);
+        $users = $mapperUsers->findAll();
+
+        /** @var \Ecommerce\V1\Rest\Users\UsersEntity $user */
+        foreach ($users as $user) {
+            $found = true;
+
+            foreach ($table->getRows() as $property) {
+                $getter = 'get' . ucfirst($property[0]);
+
+                if ($property[1] != $user->$getter()) {
+                    $found = false;
+                    break;
+                }
+            }
+            if ($found) {
+                return;
+            }
+        }
+        throw new \Exception("The $entityName should exist.");
     }
 }
--- features/users.feature
+++ features/users.feature
@@ -30,3 +30,45 @@ Feature: Users
       | username  | batman |
       | firstname | Bruce  |
       | lastname  | Wayne  |
+
+  Scenario: Retrieve the list of users
+    Given a user:
+      | username  | batman |
+      | firstname | Bruce  |
+      | lastname  | Wayne  |
+    Given a user:
+      | username  | superman |
+      | firstname | Clark    |
+      | lastname  | Kent     |
+    When I send GET request to "/users"
+    Then response status code should be 200
+    And response should be a collection of "users"
+    And response collection should contain exactly 2 "users"
+    And response collection "users" should contain the resource:
+      | username  | batman |
+      | firstname | Bruce  |
+      | lastname  | Wayne  |
+    And response collection "users" should contain the resource:
+      | username  | superman |
+      | firstname | Clark    |
+      | lastname  | Kent     |
+
+  Scenario: Delete a user
+    Given a user:
+      | username  | batman |
+      | firstname | Bruce  |
+      | lastname  | Wayne  |
+    Given a user:
+      | username  | superman |
+      | firstname | Clark    |
+      | lastname  | Kent     |
+    When I send DELETE request to "/users/batman"
+    Then response status code should be 204
+    And the "user" with the following data should have been deleted:
+      | username  | batman |
+      | firstname | Bruce  |
+      | lastname  | Wayne  |
+    But the "user" with the following data should exist:
+      | username  | superman |
+      | firstname | Clark    |
+      | lastname  | Kent     |
